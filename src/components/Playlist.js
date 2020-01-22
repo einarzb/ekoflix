e2b15efba966a1a075c989b2cafe4d281a5ef06e
einarzb <einarzb@gmail.com>
2020-01-22 16:43:17 +0200

    some fixing for edge

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+
 import { BlackShadow, EkoPink, DarkBlue } from "../assets/theme";
 import { PLAY } from "../assets/svgIndex";
 
